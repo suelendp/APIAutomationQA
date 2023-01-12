a50a8c6ce21020d7bc0d48f773c96291ced6617c
Suélen Dias Palhares <suelenddf@gmail.com>
2023-01-12 16:00:52 -0300

    Final Commit

---
+++ src/main/java/Entities/User.java
@@ -0,0 +1,64 @@
+package Entities;
+
+public class User {
+
+
+
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String phone;
+
+    public User(String username, String firstName, String lastName, String password, String phone, String s) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+
+}
